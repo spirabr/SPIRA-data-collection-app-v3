11f236ba6bcdbd7a73c0ebbd9f5b3bd82e3507e7
HigorMauricio <higormauriciosilva@gmail.com>
2026-01-17 15:40:56 -0300

    fix: correção do caso de teste

--- test/feature/presentation/cubits/data_collection/data_collection_cubit_test.dart
+++ test/feature/presentation/cubits/data_collection/data_collection_cubit_test.dart
@@ -14,20 +14,29 @@ import 'package:spira/feature/domain/entities/line_of_study_entity.dart';
 import 'package:spira/feature/domain/entities/smoking_entity.dart';
 import 'package:spira/feature/presentation/cubits/data_collection/data_collection_cubit.dart';
 
+import 'package:spira/feature/domain/entities/recorder_audio_entity.dart';
+import 'package:spira/feature/data/model/response/audio_response_model.dart';
+
 class MockLocalDataSource extends Mock implements LocalDataSource {}
+
 class MockConnectivityService extends Mock implements ConnectivityService {}
+
 class FakePatientModel extends Fake implements PatientModel {}
 
-void main(){
+void main() {
   late DataCollectionCubit cubit;
   late MockLocalDataSource mockLocalDataSource;
   late MockConnectivityService mockConnectivityService;
 
-  setUpAll( () {
+  setUpAll(() {
     registerFallbackValue(FakePatientModel());
+
+    registerFallbackValue(RecordedAudioEntity(duration: 0, path: 'dummy_path'));
+
     registerFallbackValue(DataCollectionStep.patientDataStepOne);
+    registerFallbackValue(false);
   });
-  
+
   setUp(() async {
     await GetIt.I.reset();
 
@@ -38,22 +47,24 @@ void main(){
     GetIt.I.registerSingleton<ConnectivityService>(mockConnectivityService);
 
     when(() => mockLocalDataSource.getGenders()).thenAnswer((_) async => []);
-    when(() => mockLocalDataSource.getFirstLoginResponse()).thenAnswer((_) async => null);
-    when(() => mockConnectivityService.isConnected()).thenAnswer((_) async => false);
+    when(() => mockLocalDataSource.getFirstLoginResponse())
+        .thenAnswer((_) async => null);
+    when(() => mockConnectivityService.isConnected())
+        .thenAnswer((_) async => false);
 
-    when(() => mockLocalDataSource.insertPatient(
-        any(), any(), any(), any(), any(), any(), any(), any(), any(), any()
-    )).thenAnswer((_) async => 1);
+    when(() => mockLocalDataSource.insertPatient(any(), any(), any(), any(),
+        any(), any(), any(), any(), any(), any())).thenAnswer((_) async => 1);
 
     cubit = DataCollectionCubit();
-
-    tearDown(() {
-      cubit.close();
-    });
   });
 
-  group('Testes do novo campo de Monóxido de carbono', () async {
-    test('Deve salvar Monóxido de Carbono (Double) para cigarro eletrônico', () async {
+  tearDown(() {
+    cubit.close();
+  });
+
+  group('Testes do novo campo de Monóxido de carbono', () {
+    test('Deve salvar Monóxido de Carbono (Double) para cigarro eletrônico',
+        () async {
       cubit.setLineOfStudy(LineOfStudyEntity(id: 3, name: 'Tabagismo'));
       cubit.setSmokingType(SmokingEntity(id: 2, name: 'Eletrônico'));
 
@@ -63,19 +74,31 @@ void main(){
       //inserção
       cubit.electronicCarbonMonoxideController.text = "25.5";
 
+      final dummyAudio = RecordedAudioEntity(duration: 0, path: 'dummy_path');
+
+      cubit.setAmbientNoisePath(dummyAudio);
+      cubit.setSustentainedVowelPath(dummyAudio);
+      cubit.setRhymePath(dummyAudio);
+
       //ação
       await cubit.submitCollectionData();
 
       //verificação
       final capturedCall = verify(() => mockLocalDataSource.insertPatient(
-          captureAny(), any(), any(), any(), any(), any(), any(), any(), any(), any()
-      ));
+          captureAny(),
+          any(),
+          any(),
+          any(),
+          any(),
+          any(),
+          any(),
+          any(),
+          any(),
+          any()));
 
       final savedPatient = capturedCall.captured.first as PatientModel;
 
       expect(savedPatient.smokingData?.carbonMonoxide, 25.5);
     });
-
   });
-
 }
